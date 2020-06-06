2c8debd65e52fb85952cfb7f23c04dcd2f389daa
Jacob Adams <jacob@tookmund.com>
2020-06-06 17:27:55 -0400

    Rewrite with promises and path regex

--- s3-id.js
+++ s3-id.js
@@ -1,47 +1,46 @@
-const AWS = require("aws-sdk")
+const AWS = require("aws-sdk");
 
-var ids = null
-
-function loadids() {
-	var s3 = new AWS.S3()
-	s3.getObject({
-		"Bucket": "lambda-id",
-		"Key": "ids.json",
-	}, function (err, data) {
-		if (err) {
-			ids = null
-			return { "status": false, "error": err }
-		}
-		else {
-			ids = data
-			return { "status": true }
-		}
-	});
-}
+var ids = null;
 
 function error(status, body) {
 	return {
 		"statusCode": status,
 		"isBase64Encoded": false,
 		"body": body
-	}
-
+	};
 }
 
 exports.handler = async function(event, context) {
+	let ret = null;
 	if (ids == null) {
-		let lids = loadids()
-		if (!lids["status"]) {
-			return error(500, "Unable to load data! "+JSON.stringify(lids["error"], null, "\t"))
-		}
+		let s3 = new AWS.S3();
+		let promise = s3.getObject({
+			"Bucket": "lambda-id",
+			"Key": "ids.json",
+		}).promise();
+		promise.then(
+			function(data) {
+				ids = JSON.parse(data.Body.toString());
+			},
+			function(err) {
+				ret = err;
+			});
+		await promise;
+		if (ret != null) error(500, ret);
 	}
+
 	if (event["path"] == undefined) {
 		return {
 			"statusCode": 404,
 			"isBase64Encoded": false,
 			"body":"Must be called as an API!"
-		}
+		};
 	}
+	let find = event["path"].match(/^\/ids(?:\/(\d+)(?:\/(\w+))?)?(?:\/)?$/);
+	if (find == null) return error(400, "Bad Request");
+	if (find[1] == null) ret = ids;
+	else if (find[2] == null) ret = ids[find[1]];
+	else ret = ids[find[1]][find[2]];
 	return {
 		"statusCode": 200,
 		"statusDescription": "200 OK",
@@ -50,6 +49,6 @@ exports.handler = async function(event, context) {
 			"Content-Type": "text/json",
 			"X-Clacks-Overhead:": "GNU Terry Pratchett"
 		},
-		"body": ids
+		"body": JSON.stringify(ret)
 	}
 }

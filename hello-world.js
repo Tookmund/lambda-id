ed7c1bf40dbb890b8cf20d4cfd9482761a61eee7
Jacob Adams <jacob@tookmund.com>
2020-06-06 14:19:38 -0400

    Stringify the event and add Clacks headers

--- hello-world.js
+++ hello-world.js
@@ -6,8 +6,9 @@ exports.handler = async function(event, context) {
 		"statusDescription": "200 OK",
 		"isBase64Encoded": false,
 		"headers": {
-			"Content-Type": "text/html"
+			"Content-Type": "text/html",
+			"X-Clacks-Overhead:": "GNU Terry Pratchett"
 		},
-		"body": "<h1>Hello from Lambda!</h1>"
+		"body": "<h1>Hello from Lambda!</h1><p>Hopefully I'll be the only one who can run this</p><p>"+JSON.stringify(event)+"</p>"
 	}
 }
